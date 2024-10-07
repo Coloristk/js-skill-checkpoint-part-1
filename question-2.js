cdd03ad4d7ca7d97694336c1a98d37a50a78e9c8
Kong Thiraphat <thiraphat.cto@gmail.com>
2024-10-07 14:42:28 +0700

    feat: add answer to exam_2.3

--- question-2.js
+++ question-2.js
@@ -7,3 +7,8 @@ const inventory = [
 inventory[0].quantity = 200;
 inventory.push ({name: "Orange", price: 20, quantity: 300});
 console.log(inventory) 
+
+let totalPrice = 0
+for (let i of inventory){
+  totalPrice += i.price * i.quantity
+}console.log(`มูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก ${totalPrice} บาท`)
